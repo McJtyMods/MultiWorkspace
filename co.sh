7089409bad5fe9baafdf59802910773827139535
McJty <mcjty1@gmail.com>
2023-01-25 05:25:59 +0100

    Ported to 1.19.2

---
+++ co.sh
@@ -0,0 +1,90 @@
+git clone https://github.com/McJtyMods/DeepResonance DeepResonance
+cd DeepResonance
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/FancyTrinkets Trinkets
+cd FancyTrinkets
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/LostCities LostCities
+cd LostCities
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/McJtyLib McJtyLib
+cd McJtyLib
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/XNet ModXNet
+cd ModXNet
+git checkout 1.19
+cd ..
+git clone https://github.com/romelo333/notenoughwands1.8.8 NEW
+cd NEW
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/Nice Nice
+cd Nice
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/Restrictions Restrictions
+cd Restrictions
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsBase RFToolsBase
+cd RFToolsBase
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsBuilder RFToolsBuilder
+cd RFToolsBuilder
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsControl RFToolsControl
+cd RFToolsControl
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsDimensions RFToolsDimensions
+cd RFToolsDimensions
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsPower RFToolsPower
+cd RFToolsPower
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsStorage RFToolsStorage
+cd RFToolsStorage
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/RFToolsUtility RFToolsUtility
+cd RFToolsUtility
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/SignTastic SignTastic
+cd SignTastic
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/InControl InControl
+cd InControl
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/FxControl FxControl
+cd FxControl
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/HoloGui ModHoloGui
+cd ModHoloGui
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/LostSouls LostSouls
+cd LostSouls
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/LostRuins LostRuins
+cd LostRuins
+cd ..
+git clone https://github.com/McJtyMods/Wastify Wastify
+cd Wastify
+git checkout 1.19
+cd ..
+git clone https://github.com/McJtyMods/ParsMinima ParsMinima
+cd ParsMinima
+cd ..
