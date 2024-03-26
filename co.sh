7f35d356c46abcfd95695ec428dcd87413add586
Jorrit Tyberghein <jorrit.tyberghein@nexuzhealth.com>
2024-03-26 15:49:07 +0100

    More porting work

--- co.sh
+++ co.sh
@@ -1,124 +1,125 @@
 git clone https://github.com/McJtyMods/DeepResonance DeepResonance
 pushd DeepResonance
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/FancyTrinkets Trinkets
-pushd FancyTrinkets
-git checkout 1.20
+pushd Trinkets
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/LostCities LostCities
 pushd LostCities
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/LostEdit LostEdit
 pushd LostEdit
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/McJtyLib McJtyLib
 pushd McJtyLib
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/XNet ModXNet
 pushd ModXNet
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/romelo333/notenoughwands1.8.8 NEW
 pushd NEW
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/Nice Nice
 pushd Nice
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/Restrictions Restrictions
 pushd Restrictions
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsBase RFToolsBase
 pushd RFToolsBase
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsBuilder RFToolsBuilder
 pushd RFToolsBuilder
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsControl RFToolsControl
 pushd RFToolsControl
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsDimensions RFToolsDimensions
 pushd RFToolsDimensions
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsPower RFToolsPower
 pushd RFToolsPower
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsStorage RFToolsStorage
 pushd RFToolsStorage
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/RFToolsUtility RFToolsUtility
 pushd RFToolsUtility
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/SignTastic SignTastic
 pushd SignTastic
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/InControl InControl
 pushd InControl
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/FxControl FxControl
 pushd FxControl
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/HoloGui ModHoloGui
 pushd ModHoloGui
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/LostSouls LostSouls
 pushd LostSouls
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/LostRuins LostRuins
 pushd LostRuins
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
-git clone https://github.com/McJtyMods/Wastify Wastify
-pushd Wastify
-git checkout 1.20
+git clone https://github.com/McJtyMods/LostWorlds LostWorlds
+pushd LostWorlds
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/InteractionWheel
 pushd InteractionWheel
-git checkout 1.20
+git checkout 1.20.4_neo
 popd
 
 git clone https://github.com/McJtyMods/ParsMinima ParsMinima
 pushd ParsMinima
+git checkout 1.20.4_neo
 popd
 
